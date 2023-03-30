ef96368314a3e6ec4994553053aa8e8a4efe8d8b
akash1kumar20 <127503323+akash1kumar20@users.noreply.github.com>
2023-03-30 13:37:24 +0530

    changes in the script of the income tax calculator

--- brain.js
+++ brain.js
@@ -52,10 +52,31 @@ display.appendChild(newButton);
     
 newButton.onclick = () =>{
 
+    let attribute = () =>{
+      if(toShow===0){
+        messeagePop = 'Type your salary here less then 3 LPA...';
+      }
+      else if(toShow===5){
+        messeagePop = 'Type your salary here in between 3 to 6 LPA...';
+      }
+      else if(toShow===10){
+        messeagePop = 'Type your salary here in between 6 to 9 LPA...';
+      }
+      else if(toShow===15){
+        messeagePop = 'Type your salary here in between 9 to 12 LPA...';
+      }
+      else if(toShow===20){
+        messeagePop = 'Type your salary here in between 12 to 15 LPA...';
+      }
+      else if(toShow===30){
+        messeagePop = 'Type your salary here above then 15 LPA...';
+      }
+    }
+    attribute();
+
     let valueDisplay = document.createElement('input');
     valueDisplay.setAttribute = ('type', 'text');
-    valueDisplay.placeholder = 'Type your salary here in LPA...'
-    console.log(valueDisplay);
+    valueDisplay.placeholder = messeagePop;
     valueDisplay.className= 'type-text mt-2 mb-2';
     valueDisplay.id = 'id-one'
     let otherButton = document.createElement('button');
@@ -102,13 +123,13 @@ newButton.onclick = () =>{
         let newDiv = document.createElement('div');
         newDiv.textContent = 'Amount deducted on your salary is ' + amount;
         newDiv.className = 'mt-2 mb-2';
-        newDiv.style.border = '1px solid red';
+        newDiv.style.border = '2px solid red';
         display.appendChild(newDiv);
         
         let otherDiv = document.createElement('div');
         otherDiv.textContent = 'Final salary after deduction is ' + salaryLeft;
         otherDiv.className = 'mt-2 mb-2'
-        otherDiv.style.border = '1px solid green';
+        otherDiv.style.border = '2px solid green';
         display.appendChild(otherDiv);
         
         console.log('Amount',amount);
